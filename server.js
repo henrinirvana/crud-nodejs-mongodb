93577581bd498ee024f98b712fcbde65853d74b4
henrinirvana <contato_antoniohenrique@hotmail.com>
2020-04-04 23:05:20 -0300

    commit 4

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
@@ -9,8 +11,17 @@ app.use(express.json());
 app.use(cors());
 
 //iniciando o banco de dados
+/*
 mongoose.connect(
     "mongodb://localhost:27017/nodeapi",
+    { 
+        useNewUrlParser: true,
+        useUnifiedTopology:true
+     }
+);
+*/
+mongoose.connect(
+    process.env.MONGO_URL,
     { 
         useNewUrlParser: true,
         useUnifiedTopology:true
